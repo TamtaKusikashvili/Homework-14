7d1281ba5b97e9239a7d02cf2eaa4050db10c1bf
TamtaKusikashvili <tatakusikashvili@gmail.com>
2025-12-12 14:32:33 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+//1/
+//const add = (a, b) => a + b;
+//console.log(add(5, 7)); 
+
+//2/
+//(function(a, b) {
+    return a - b;
+//})(8, 3);
+
+
+//3/
+// //const isEven = (num) => num % 2 === 0;
+//console.log(isEven(4)); 
+//console.log(isEven(9)); 
+
+//4/
+// //function calculate(a, b, callback) 
+//{return callback(a, b);}
+//const multiply = (x, y) => x * y;
+//const result = calculate(5, 3, multiply);
+//console.log(result); 
+
+//5/
+// //const isPositive = (num) => num > 0;
+//console.log(isPositive(5));  
+//console.log(isPositive(-3));  
+//console.log(isPositive(0));   
+
+//6/
+//(function(n) {return n * 2;})(7);
+
+
+ //7//
+ //function calculate(a, callback)
+ //{return callback(a);}
+// const double = (x) => x * 2;
+// const result = calculate(6, double)
+// console.log(result);
+
+//8//
+//const isDivisibleByThree = (num) => num % 3 === 0;
+//console.log(isDivisibleByThree(8));
+//console.log(isDivisibleByThree(9));
+//console.log(isDivisibleByThree(15));
+
+//9/
+//function checkNumber(num, callback) 
+//{return callback(num);}
+//const isEven = function(n) 
+//{return n % 2 === 0;};
+//const result = checkNumber(8, isEven);
+//console.log(result); 
+
+
+//10//
+//const cube = (n) => n * n * n;
+//console.log(cube(3));
+//console.log(cube(8));
+
+//11/
+//function calculate(a, b, callback) 
+//{return callback(a, b);}
+//const multiply = function(x, y) 
+//{return x * y;};
+//const result = calculate(5, 7, multiply);
+//console.log(result);
+
+//12/
+//const isGreaterThanZero = (num) => num > 0;
+//console.log(isGreaterThanZero(5));   
+//console.log(isGreaterThanZero(0));   
+//console.log(isGreaterThanZero(-3));  
+
+//13/
+//function applyCallback(num, callback) 
+//{return callback(num);}
+//const half = function(n) {return n / 2;};
+//const result = applyCallback(10, half);
+//console.log(result); 
+
+
+//14/
+//function calculateSum(a, b, callback) 
+//{return callback(a, b);}
+//const sum = function(x, y) 
+//{return x + y;};
+//const result = calculateSum(7, 8, sum);
+//console.log(result); 
+
+//15//
+//const square = (num) => num * num;
+//console.log(square(4));
